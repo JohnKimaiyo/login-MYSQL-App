b2855dbb41d4e72f869abebb930cccc7d2769b89
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-07-22 06:57:13 -0700

    final commt

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express')
+
+const mysql =  require(("mysql"))
 const app = express()
+
+const db = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:''
+})
 const port = 5000
 
 app.get('/', (req, res) => {
