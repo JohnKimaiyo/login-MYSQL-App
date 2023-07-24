c9f8aff1bd622914589dc2f5df041c4510dd0c9f
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-07-24 05:45:06 -0700

    final commit

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
-const express = require('express')
+const express = require("express");
 
-const mysql =  require(("mysql"))
-const app = express()
+const mysql = require("mysql");
+const app = express();
 
 const db = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:''
-})
-const port = 5000
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "login",
+});
+const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('<h1>Home Page')
-})
+db.connect((error) => {
+  if (error) {
+    console.log(error)
+  } else {
+    console.log("Mysql connected")
+  }
+});
+app.get("/", (req, res) => {
+  res.send("<h1>Home Page");
+});
 
 app.listen(port, () => {
-  console.log(`Server is working ${port}`)
-})
+  console.log(`Server is working ${port}`);
+});
