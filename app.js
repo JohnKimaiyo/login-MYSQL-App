3f3ca9b9cbd0616072393092c99f0666636b64eb
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-07-24 06:17:17 -0700

    final commit

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const express = require("express");
 
 const mysql = require("mysql");
+const env = require("dotenv");
+
+dotenv.config({ path: "./env" });
 const app = express();
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "login",
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABSE_USER,
+  password:process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE,
 });
 const port = 5000;
 
 db.connect((error) => {
   if (error) {
-    console.log(error)
+    console.log(error);
   } else {
-    console.log("Mysql connected")
+    console.log("Mysql connected");
   }
 });
 app.get("/", (req, res) => {
